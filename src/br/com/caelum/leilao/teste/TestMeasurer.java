f010dc433cdd35f16468fb8b388cc8b4bee39f45
Pedro Henrique <pedro.hc.silva@outlook.com>
2020-11-25 18:53:13 -0300

    #004 - Adição de novos testes para nome do user

--- src/br/com/caelum/leilao/teste/TestMeasurer.java
+++ src/br/com/caelum/leilao/teste/TestMeasurer.java
@@ -7,6 +7,7 @@ import br.com.caelum.leilao.dominio.Bid;
 import br.com.caelum.leilao.dominio.Auction;
 import br.com.caelum.leilao.dominio.User;
 import br.com.caelum.leilao.servico.Measurer;
+import org.junit.experimental.theories.suppliers.TestedOn;
 
 public class TestMeasurer {
 
@@ -94,6 +95,50 @@ public class TestMeasurer {
         User userlowestBidResult = auctioneer.getUserLowestBid();
 
         Assert.assertEquals(userhighestBidExpected, userhighestBidResult.getName());
+        Assert.assertEquals(userlowestBidExpected, userlowestBidResult.getName());
+    }
+
+    @Test
+    public void userHighestBid() {
+        User joao = new User("João Pedro");
+        User jose = new User("José Marcos");
+        User maria = new User("Maria Madalena");
+
+        Auction auction = new Auction("Playstation 5");
+
+        auction.setBid(new Bid(joao, 5000.0));
+        auction.setBid(new Bid(jose, 3250.0));
+        auction.setBid(new Bid(maria, 5001.0));
+
+        Measurer auctioneer = new Measurer();
+        auctioneer.rate(auction);
+
+        String userhighestBidExpected = "Maria Madalena";
+
+        User userhighestBidResult = auctioneer.getUserHighestBid();
+
+        Assert.assertEquals(userhighestBidExpected, userhighestBidResult.getName());
+    }
+
+    @Test
+    public void userLowestBid() {
+        User joao = new User("Pedro Henrique");
+        User jose = new User("Hyago Souza");
+        User maria = new User("João Pedro");
+
+        Auction auction = new Auction("Playstation 5");
+
+        auction.setBid(new Bid(joao, 3201.0));
+        auction.setBid(new Bid(jose, 4600.0));
+        auction.setBid(new Bid(maria, 4000.0));
+
+        Measurer auctioneer = new Measurer();
+        auctioneer.rate(auction);
+
+        String userlowestBidExpected = "Pedro Henrique";
+
+        User userlowestBidResult = auctioneer.getUserLowestBid();
+
         Assert.assertEquals(userlowestBidExpected, userlowestBidResult.getName());
     }
 }
